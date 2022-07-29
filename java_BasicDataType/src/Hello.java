72daefb4e5d3f1e430e1b46e20443d06a2654502
codeking <1094234526@qq.com>
2022-07-29 10:06:00 +0800

    javaSE，狂神的，笔记https://www.kuangstudy.com/bbs/1531983845413756929

---
+++ java_BasicDataType/src/Hello.java
@@ -0,0 +1,8 @@
+/**
+ * @author king
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ java_BasicDataType/src/test.java
@@ -0,0 +1,6 @@
+/**
+ * @author : codeking
+ * @date : 2022/7/28 17:52
+ */
+public class test {
+}
